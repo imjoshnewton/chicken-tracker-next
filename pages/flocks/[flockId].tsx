5a87f6904389b0db812af6fc8f83363692302022
Josh Newton <josh.newton@me.com>
2022-08-05 23:36:03 -0400

    Show loader when no flockId

--- pages/flocks/[flockId].tsx
+++ pages/flocks/[flockId].tsx
@@ -1,23 +1,39 @@
-import { doc } from "firebase/firestore";
+import { collection, doc, limit, orderBy, query, where } from "firebase/firestore";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import { useDocumentData } from 'react-firebase-hooks/firestore';
+import { useCollectionData, useDocumentData } from 'react-firebase-hooks/firestore';
 import { firestore } from "../../libs/firebase";
 import Card from "../../components/Card";
 import Loader from "../../components/Loader";
+import Breeds from "../../components/Breeds";
+import Stats from "../../components/Stats";
+import { useEffect } from "react";
 
 export default function Flocks({ params }) {
     const router = useRouter();
     const { flockId } = router.query;
-    const flockDoc = doc(firestore, `flocks/${flockId}`);
-    const [flockData, loading, error] = useDocumentData(flockDoc);
+
+    if (!flockId) {
+        return (
+            <main>
+                <Loader show={true}></Loader>
+            </main>
+        );
+    }
+
+    let flockDoc = doc(firestore, `flocks/${flockId}`);
+    let [flockData, loading, error] = useDocumentData(flockDoc);
+
+    let logCol = collection(firestore, 'logs');
+    let q = query(logCol, where('flock', '==', flockId), orderBy('date', 'desc'), limit(7));;
+    let [logData] = useCollectionData(q);
 
     return (
         !loading 
             ? error ? (<main><p>{error.message}</p></main>) 
                 : (
                     <main>
-                        {flockId}<br />
+                        {/* {flockId}<br /> */}
                         <Card title="Flock Details">
                             <div className="d-flex align-items-center">
                                 <Image src={flockData.imageUrl} width="150" height="150" className="flock-image" alt="" />
@@ -28,26 +44,9 @@ export default function Flocks({ params }) {
                                 </div>
                             </div>
                             <div className="divider my-4"></div>
-                            <div>
-                                <h3>Breeds</h3>
-                                <div className="d-flex flex-wrap">
-                                {
-                                    flockData.chickens.map((breed, index) => {
-                                        return (
-                                            <div className="d-flex align-items-center breed" key={index}>
-                                                <Image src={breed.imageUrl} width="50" height="50" className="flock-image" alt="" />
-                                                <div className="ms-3">
-                                                    <p>
-                                                        <strong>{breed.breed}</strong>
-                                                        <br/>
-                                                        <strong>Count: </strong>{breed.count}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        )
-                                    })
-                                }
-                                </div>
+                            <div className="d-flex flex-wrap justify-content-evently">
+                                <Breeds breeds={flockData.chickens} className="flex-50"></Breeds>
+                                <Stats logs={logData} flock={flockData} className="flex-50"></Stats>
                             </div>
                         </Card>
                     </main>
