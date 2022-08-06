fa7a1ad771334a2cfe1dac74afd585bc68b1d676
Josh Newton <josh.newton@me.com>
2022-08-05 23:35:12 -0400

    promote breeds info to component

---
+++ components/Breeds.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export default function Breeds({ breeds, className }) {
+    return (
+        <div className={className}>
+            <h3>Breeds</h3>
+            <div className="d-flex flex-wrap">
+                {
+                    breeds.map((breed, index) => {
+                        return (
+                            <div className="d-flex align-items-center breed" key={index}>
+                                <Image src={breed.imageUrl} width="50" height="50" className="flock-image" alt="" />
+                                <div className="ms-3">
+                                    <p>
+                                        <strong>{breed.breed}</strong>
+                                        <br />
+                                        <strong>Count: </strong>{breed.count}
+                                    </p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
+}
